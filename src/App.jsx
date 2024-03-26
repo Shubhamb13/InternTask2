cc8b6a011504be1e81356c1e930ea3d7910b1e39
Shubham <bhadarageshubham@gmail.com>
2024-03-27 01:21:09 +0530

    first change

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lightcom from './Intern/Lightcom'
+import Lighthome from './Intern/Lighthome'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Lighthome/>
+       <Lightcom/>
+    </div>
+  )
+}
+
+export default App
